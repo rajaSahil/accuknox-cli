6a306d00e60ddd8ab1354950f68cc7423bbca62b
sahil <sahilraja242@gmail.com>
2023-06-21 22:04:36 +0530

    feat(report): add report cli
    Signed-off-by: sahil <sahilraja242@gmail.com>

---
+++ cmd/report.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/accuknox/accuknox-cli/report"
+	"github.com/spf13/cobra"
+)
+
+var reportOptions report.Options
+
+var reportCmd = &cobra.Command{
+	Use:   "report",
+	Short: "Report from discovery engine",
+	Long:  `Discovery engine keeps the telemetry information from the policy enforcement engines and the karmor connects to it to provide this as observability data`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := reportOptions.Report(client); err != nil {
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(reportCmd)
+
+	reportCmd.Flags().StringVar(&reportOptions.GRPC, "gRPC", "", "gRPC server information")
+	reportCmd.Flags().StringArrayVarP(&reportOptions.Clusters, "clusters", "c", []string{""}, "Clusters names")
+	reportCmd.Flags().StringArrayVarP(&reportOptions.Namespaces, "namespaces", "n", []string{""}, "Namespaces names")
+	reportCmd.Flags().StringArrayVarP(&reportOptions.ResourceType, "resource-types", "r", []string{""}, "Resource types")
+	reportCmd.Flags().StringArrayVarP(&reportOptions.ResourceName, "resource-names", "w", []string{""}, "Resource names")
+	reportCmd.Flags().StringVarP(&reportOptions.Labels, "labels", "l", "", "Labels")
+	reportCmd.Flags().StringVar(&reportOptions.ContainerName, "container", "", "Container name")
+	reportCmd.Flags().StringArrayVarP(&reportOptions.Source, "source", "s", []string{""}, "Source path")
+	reportCmd.Flags().StringArrayVarP(&reportOptions.Destination, "destination", "d", []string{""}, "Destination path")
+	reportCmd.Flags().StringVar(&reportOptions.Operation, "o", "", "Operation type")
+}
---
+++ report/report.go
@@ -0,0 +1,120 @@
+package report
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/accuknox/accuknox-cli/k8s"
+	rpb "github.com/accuknox/accuknox-cli/rpb"
+
+	"github.com/accuknox/accuknox-cli/utils"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"os"
+	"strconv"
+)
+
+// DefaultReqType : default option for request type
+var DefaultReqType = "process,file,network"
+var matchLabels = map[string]string{"app": "discovery-engine"}
+var port int64 = 9089
+
+type Options struct {
+	GRPC          string
+	Clusters      []string
+	Namespaces    []string
+	ResourceType  []string
+	ResourceName  []string
+	Labels        string
+	ContainerName string
+	PodName       string
+	Source        []string
+	Destination   []string
+	Operation     string
+}
+
+func GetReport(c *k8s.Client, o *Options) (*rpb.ReportResponse, error) {
+	gRPC := ""
+	targetSvc := "discovery-engine"
+	if o.GRPC != "" {
+		gRPC = o.GRPC
+	} else {
+		if val, ok := os.LookupEnv("DISCOVERY_SERVICE"); ok {
+			gRPC = val
+		} else {
+			pf, err := utils.InitiatePortForward(c, port, port, matchLabels, targetSvc)
+			if err != nil {
+				return nil, err
+			}
+			gRPC = "localhost:" + strconv.FormatInt(pf.LocalPort, 10)
+		}
+	}
+
+	req := &rpb.ReportRequest{
+		Clusters:     o.Clusters,
+		Namespaces:   o.Namespaces,
+		ResourceType: o.ResourceType,
+		ResourceName: o.ResourceName,
+		PodName:      o.PodName,
+		MetaData: &rpb.MetaData{
+			Label:         o.Labels,
+			ContainerName: o.ContainerName,
+		},
+		Operation:   o.Operation,
+		Source:      o.Source,
+		Destination: o.Destination,
+	}
+
+	// create a client
+	conn, err := grpc.Dial(gRPC, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, errors.New("could not connect to the server. Possible troubleshooting:\n- Check if discovery engine is running\n- kubectl get po -n accuknox-agents")
+	}
+	defer conn.Close()
+
+	client := rpb.NewReportClient(conn)
+
+	res, err := client.GetReport(context.Background(), req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (o *Options) Report(c *k8s.Client) error {
+
+	report, err := GetReport(c, o)
+	if err != nil {
+		log.Error().Msgf("error while getting report, error: %s", err.Error())
+		return err
+	}
+	reportJson, err := json.MarshalIndent(report, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s \n", reportJson)
+
+	// Write in a temp file
+	tempF, err := os.CreateTemp("/tmp/", "report-*.json")
+	if err != nil {
+		return err
+	}
+
+	if _, err := tempF.Write(reportJson); err != nil {
+		return err
+	}
+
+	baselineReport := readBaselineReportJson("/home/sahil/report.json")
+
+	err = getDiff(baselineReport, report, []string{})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
