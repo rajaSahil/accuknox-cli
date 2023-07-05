6dbb482775f536029c82fcd825c5d779ddda2b91
sahil <sahilraja242@gmail.com>
2023-07-06 02:08:08 +0530

    feat(report): update report
    Signed-off-by: sahil <sahilraja242@gmail.com>

--- cmd/report.go
+++ cmd/report.go
@@ -28,11 +28,16 @@ func init() {
 	reportCmd.Flags().StringSliceVarP(&reportOptions.ResourceType, "resource-types", "r", []string{}, "Resource types")
 	reportCmd.Flags().StringSliceVarP(&reportOptions.ResourceName, "resource-names", "w", []string{}, "Resource names")
 	reportCmd.Flags().StringVarP(&reportOptions.Labels, "labels", "l", "", "Labels")
-	reportCmd.Flags().StringVar(&reportOptions.ContainerName, "container", "", "Container name")
+	reportCmd.Flags().StringVar(&reportOptions.ContainerName, "container", "c", "Container name")
 	reportCmd.Flags().StringSliceVarP(&reportOptions.Source, "source", "s", []string{}, "Source path")
 	reportCmd.Flags().StringSliceVarP(&reportOptions.Destination, "destination", "d", []string{}, "Destination path")
-	reportCmd.Flags().StringVarP(&reportOptions.Operation, "operation", "o", "", "Operation type")
+	reportCmd.Flags().StringVarP(&reportOptions.Operation, "operation", "t", "", "Operation type")
 	reportCmd.Flags().StringSliceVarP(&reportOptions.IgnorePaths, "ignore-paths", "i", []string{}, "Destination path")
 
 	reportCmd.Flags().StringVarP(&reportOptions.BaselineJsonPath, "baseline-json-filepath", "b", "baseline/report.json", "Operation type")
+
+	//reportCmd.Flags().StringVarP(&reportOptions.OutputMode, "output-mode", "m", "diff", "Output mode")
+	//reportCmd.Flags().StringVarP(&reportOptions.OutputFormat, "output-format", "o", "table", "Output format, can be table, json or markdown")
+	//reportCmd.Flags().StringVarP(&reportOptions.OutputFileName, "output-file", "f", "", "Output file for report json")
+
 }
